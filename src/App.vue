a794923f0d94126b7567e8319bcb7f13747a333f
eliooooooo <eliott.burkle@gmail.com>
2024-08-21 12:10:02 +0200

    Feat: Display movies

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from './components/Table.vue';
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <div>
+    <h1>Liste des films</h1>
+    <Table></Table>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Plot</th>
+                <th scope="col">Genres</th>
+                <th scope="col">Poster</th>
+                <th scope="col">Released</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="movie in movies" :key="movie._id">
+                <td>{{ movie.title }}</td>
+                <td>{{ movie.plot }}</td>
+                <td>{{ movie.genres.join(', ') }}</td>
+                <td><img :src="movie.poster" alt="Poster"></td>
+                <td>{{ movie.released }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            movies: []
+        };
+    },
+    created() {
+        this.fetchMovies();
+    },
+    methods: {
+        async fetchMovies() {
+            try {
+                console.log('Fetching movies...');
+                const response = await axios.get('http://localhost:3000/movies');
+                console.log('Movies fetched:', response.data);
+                this.movies = response.data;
+            } catch (error) {
+                console.error('Erreur lors de la récupération des films:', error);
+            }
+        }
+    }
+};
+</script>
+
+<style scoped></style>
