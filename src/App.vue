4d7978b70d4188864dc791ca1b6feac2c4b0f4f2
eliooooooo <eliott.burkle@gmail.com>
2024-08-23 12:26:47 +0200

    Docs: add todo

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import Table from './components/Table.vue';
 import Pagination from './components/Pagination.vue';
 
+// La pagination passe encore par un refresh de page, il faudrait passer la page en props
+// ainsi que de relancer la requete à l'api pour récupérer les films de la page demandée
 let params = new URLSearchParams(document.location.search);
 let currentPage = params.get("page") ? parseInt(params.get("page"), 10) : 1;
 </script>
